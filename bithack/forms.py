cea8cb7b33847b12a558b025ee7df4b226df02dd
Max Fang <maxfangx@gmail.com>
2015-07-16 16:25:24 -0700

    Add registration form

---
+++ bithack/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+
+class RegisterForm(forms.Form):
+    
+    TSHIRT_SIZE_CHOICES = (
+        ('extra small', "Adult Extra Small"),
+        ('small', "Adult Small"),
+        ('medium', "Adult Medium"),
+        ('large', "Adult Large"),
+        ('extra large', "Adult Extra Large"),
+    )
+
+    name = forms.CharField(label="Full Name", max_length=100)
+    # TODO change this to email field
+    email = forms.CharField(label="Email", max_length=256)
+    school = forms.CharField(label="School", max_length=256)
+    tshirt_size = forms.CharField(label="T-shirt size", max_length=30, choices=TSHIRT_SIZE_CHOICES)
