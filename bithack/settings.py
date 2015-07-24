0a6dee15d1c81befa9d64bd5cb74ec955fa9c63a
Max Fang <maxfangx@gmail.com>
2015-07-24 01:04:37 -0700

    Add HEROKU_SECRET_KEY

--- bithack/settings.py
+++ bithack/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "{{ secret_key }}"
+SECRET_KEY = os.getenv("BITHACK_SECRET_KEY", "fake_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
