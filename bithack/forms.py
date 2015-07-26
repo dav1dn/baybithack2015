e9f991ea18c9e89cdafad3e44e30354ac0b5ceb6
Max Fang <maxfangx@gmail.com>
2015-07-25 20:01:03 -0700

    Fix errors in form

--- bithack/forms.py
+++ bithack/forms.py
@@ -16,6 +16,6 @@ class RegisterForm(forms.Form):
     last_name = forms.CharField(label="Last Name", max_length=100)
     email = forms.EmailField(label="Email", max_length=254)
     school = forms.CharField(label="School", max_length=256)
-    tshirt_size = forms.CharField(label="T-shirt size", max_length=30, choices=TSHIRT_SIZE_CHOICES)
-    vegetarian = forms.BooleanField(label="Vegetarian options", default=False)
+    tshirt_size = forms.ChoiceField(label="T-shirt size", choices=TSHIRT_SIZE_CHOICES)
+    vegetarian = forms.BooleanField(label="Vegetarian options")
     source = forms.CharField(label="Where did you hear about this event?", max_length=256)
