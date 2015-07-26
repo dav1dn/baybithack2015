6c0a1b6555db1b6251a21bfd5dc1ecfaf529806c
Max Fang <maxfangx@gmail.com>
2015-07-25 20:08:57 -0700

    Add forms to flow

--- bithack/views.py
+++ bithack/views.py
@@ -1,7 +1,12 @@
+from bithack.forms import RegisterForm
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
 def home(request):
-    context = {}
+    register_form = RegisterForm()
+
+    context = {'register_form': register_form}
+
     return render_to_response("home.html", context, RequestContext(request))
