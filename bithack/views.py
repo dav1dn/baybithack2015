6c5496fd5790bdc6f3db6be4e4e8b44a7a189ad2
Max Fang <maxfangx@gmail.com>
2015-07-28 23:45:30 -0700

    Form submission

--- bithack/views.py
+++ bithack/views.py
@@ -1,10 +1,16 @@
 from bithack.forms import RegisterForm
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
 def index(request):
+
+    if request.method == 'POST':
+        # TODO add handling for registrant submission
+        return HttpResponseRedirect('/success')
+
     register_form = RegisterForm()
 
     context = {'register_form': register_form}
