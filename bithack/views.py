4aa0d09cefa1546c1b96a2d0382920970ddb114a
Max Fang <maxfangx@gmail.com>
2015-08-05 19:33:14 -0700

    Finalize form submission

--- bithack/views.py
+++ bithack/views.py
@@ -1,4 +1,5 @@
 from bithack.forms import RegisterForm
+from bithack.models import Hacker
 
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseBadRequest
@@ -10,10 +11,20 @@ def index(request):
 
     if request.method == 'POST':
         # TODO make Register Form into HackerForm that's a modelForm
-        # TODO make another form for user registration to handle the user creation
         form = RegisterForm(request.POST)
         if form.is_valid():
-            User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+
+            hacker_user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            hacker = Hacker()
+            hacker.user = hacker_user
+            hacker.first_name = request.POST['first_name']
+            hacker.last_name = request.POST['last_name']
+            hacker.email = request.POST['email']
+            hacker.school = request.POST['school']
+            hacker.tshirt_size = request.POST['tshirt_size']
+            hacker.vegetarian = request.POST.get('vegetarian', False)
+            hacker.source = request.POST['source']
+            hacker.save()
             return HttpResponseRedirect('/success')
         else:
             response = HttpResponseBadRequest()
